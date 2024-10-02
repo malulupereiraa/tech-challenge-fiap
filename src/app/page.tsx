cc2e9f0ff07eb4819b6d963bacf8c327ef45b9d4
cibelesantos <cibele.santos@oitocreditoimobiliario.com.br>
2024-10-02 14:18:04 -0300

    feat: aplica o tema de cores e fonte como exemplo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { theme_color, theme_fonts} from './@theme/theme'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Tech Challenge was born!!</h1>
+    <div style={{backgroundColor: theme_color.white, color: theme_color.success}}>
+      <h1 style={theme_fonts.header_semibold}>
+        Tech Challenge was born!!
+      </h1>
     </div>  
   );
 }
