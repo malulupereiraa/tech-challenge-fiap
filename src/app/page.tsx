579a52c92d4ffa013c4504a6eb4c9296e003424e
Thiago Martins <thiagofm33@gmail.com>
2024-10-21 18:52:01 -0300

    Updated `src/app/page.tsx`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,16 @@
 'use client'
 
-
 import { Row } from "react-bootstrap";
 import Footer from "./@core/components/Home/Footer";
 import Header from "./@core/components/Home/Header";
 import Main from "./@core/components/Home/Main";
 
 export default function Home() {
-
   return (
     <Row style={{ overflowX: "hidden" }}>
       <Header />
       <Main />
       <Footer />
-
     </Row>
-
   )
 }
