bf68968f14403fab041702e7ed407b340a30d1af
Maria Luiza Pereira <maluupereiraa@gmail.com>
2024-10-25 16:43:25 -0300

    Update src/app/@core/components/Toast/index.tsx
    Co-authored-by: Thiago F. Martins <thiagofm33@gmail.com>

--- src/app/@core/components/Toast/index.tsx
+++ src/app/@core/components/Toast/index.tsx
@@ -7,50 +7,6 @@ import ToastContainer from "./ToastContainer";
 import ToastIconWrapper from "./ToastIconWrapper";
 import ToastProps from "../../props/toast";
 
-// export default ({
-//   icon = "info",
-//   message,
-//   title,
-//   autohideDelay,
-//   absolutePosition = true,
-// }: ToastProps) => {
-//   const [show, setShow] = useState(true);
-//   const autohideEnabled = autohideDelay ? true : false;
-//   const close = () => setShow(false);
-
-//   const contents: React.JSX.Element = (
-//     <BaseToast
-//       delay={autohideDelay}
-//       show={show}
-//       onClose={close}
-//       autohide={autohideEnabled}
-//     >
-//       <BaseToast.Body>
-//         <div className="toast-content">
-//           <div>
-//             <ToastIconWrapper iconName={icon} />
-//           </div>
-//           <div>
-//             {title && <h6>{title}</h6>}
-//             {message}
-//           </div>
-//         </div>
-//       </BaseToast.Body>
-//     </BaseToast>
-//   );
-
-//   return (
-//     <ToastContainer>
-//       {absolutePosition ? (
-//         <BaseToastContainer position="top-center">
-//           {contents}
-//         </BaseToastContainer>
-//       ) : (
-//         contents
-//       )}
-//     </ToastContainer>
-//   );
-// };
 
 const ToastTCF: React.FC<ToastProps> = ({
   icon = "info",
