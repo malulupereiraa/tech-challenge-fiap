09467c1bb8b186c9a9dbf3cfefacf78f8b32e058
Maria Luiza Pereira <maluupereiraa@gmail.com>
2024-10-25 16:41:55 -0300

    Update next.config.mjs
    Co-authored-by: Thiago F. Martins <thiagofm33@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  // webpack(config, options) {
-  //   config.resolve.alias.canvas = false
-  //   config.plugins.push(
-  //     new NextFederationPlugin({
-  //       name: 'algodao',
-  //       filename: 'static/chunks/remoteEntry.js',
-  //       remotes: remotes(options.isServer),
-  //       exposes: {
-  //         './algodaoApp': './pages/index.js',
-  //       },
-  //     }),
-  //   )
-  //   return config
-  // },
 };
 
 export default nextConfig;
