e0b36dab724d7ce65e1f15bea1cec6ee36157777
Flavia Jaconis <fla_njaconis@yahoo.com.br>
2024-10-28 20:13:25 -0300

    Delete imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import { Row } from "react-bootstrap";
 import Footer from "./@core/components/Home/Footer";
 import Header from "./@core/components/Home/Header";
 import Main from "./@core/components/Home/Main";
-import TransactionsHeader from "./@core/components/ui/header-transactions/TransactionsHeader";
-import Menu from "./@core/components/ui/menu/Menu";
+
 
 export default function Home() {
   return (
