fc81c4ada8d71a61869722771ab7880fee0677f7
Thiago Martins <thiagofm33@gmail.com>
2024-10-31 21:22:35 -0300

    Fixed the statement component's loading

--- src/app/@core/components/Statement/index.tsx
+++ src/app/@core/components/Statement/index.tsx
@@ -41,6 +41,9 @@ export default ({ transactions, loading }: Props) => {
   };
 
   const filteredTransactions = () => {
+    if (transactions === undefined)
+      return [];
+
     if (filter == "positive")
       return transactions.filter((transaction) => transaction.amount > 0);
 
@@ -79,7 +82,7 @@ export default ({ transactions, loading }: Props) => {
     return <FaArrowLeftLong />;
   };
 
-  const placeholder = () => {
+  const placeholder = (): JSX.Element => {
     return (
       <div className="section-placeholder">
         {[1, 2].map(() => (
@@ -137,6 +140,7 @@ export default ({ transactions, loading }: Props) => {
         ? placeholder()
         : Object.entries(transactionsByMonth()).map((monthTransactions) => (
             <StatementSection
+              key={monthTransactions[0]}
               month={monthTransactions[0]}
               items={monthTransactions[1] as StatementItemProps[]}
             />
