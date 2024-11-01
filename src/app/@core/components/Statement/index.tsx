dbe2f3956f663c9218cbed4f544bd54d50b20197
Thiago Martins <thiagofm33@gmail.com>
2024-11-01 07:37:43 -0300

    Fixed the date format problem in the statement component

--- src/app/@core/components/Statement/index.tsx
+++ src/app/@core/components/Statement/index.tsx
@@ -19,9 +19,12 @@ export default ({ transactions, loading }: Props) => {
     transactionA: StatementItemProps,
     transactionB: StatementItemProps
   ) => {
-    if (transactionA.date < transactionB.date) return sortDirection * -1;
+    const timestampA = Date.parse(transactionA.date as string);
+    const timestampB = Date.parse(transactionB.date as string);
 
-    if (transactionA.date > transactionB.date) return sortDirection * 1;
+    if (timestampA < timestampB) return sortDirection * -1;
+
+    if (timestampA > timestampB) return sortDirection * 1;
 
     return 0;
   };
@@ -55,14 +58,13 @@ export default ({ transactions, loading }: Props) => {
 
   const transactionsByMonth = () => {
     const transactionsWithParsedDate = filteredTransactions()
+      .sort(sortByDate)
       .map((transaction: StatementItemProps) => {
-        if (typeof transaction.date !== "string") return transaction;
-
         return Object.assign(transaction, {
-          date: new Date(`${transaction.date}T00:00:00`),
+          date: new Date(transaction.date),
         });
       })
-      .sort(sortByDate);
+      .slice(0,9);
 
     return Object.groupBy(
       transactionsWithParsedDate,
